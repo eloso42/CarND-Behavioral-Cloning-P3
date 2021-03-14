b1a0e32d1b25da16da70b7d01313fbed898528e2
eloso42 <eloso42@gmx.de>
2021-03-14 22:47:37 +0100

    add normalization, cropping, and mirroring

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 lines = []
 hasFirst = False
-with open('../data/bhc/driving_log.csv') as csvFile:
+with open('../data/driving_log.csv') as csvFile:
     reader = csv.reader(csvFile)
     for line in reader:
         if not hasFirst:
@@ -17,24 +17,33 @@ measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = '../data/bhc/IMG/' + filename
+    current_path = '../data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
+    image_flipped = np.fliplr(image)
+    measurement_flipped = -measurement
+    images.append(image_flipped)
+    measurements.append(measurement_flipped)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 model.save('model.h5')
