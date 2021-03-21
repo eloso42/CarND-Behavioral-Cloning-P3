5ad74e2524cc9a0f58ea825ad5e9ccd417b0a617
eloso42 <eloso42@gmx.de>
2021-03-21 12:00:21 +0100

    add left and right images, update model

--- model.py
+++ model.py
@@ -4,7 +4,8 @@ import numpy as np
 
 lines = []
 hasFirst = False
-with open('../data/driving_log.csv') as csvFile:
+#with open('../data/driving_log.csv') as csvFile:
+with open('templates/driving_log.csv') as csvFile:
     reader = csv.reader(csvFile)
     for line in reader:
         if not hasFirst:
@@ -12,18 +13,54 @@ with open('../data/driving_log.csv') as csvFile:
             continue
         lines.append(line)
 
+
+def loadImage(filename):
+    filename = filename.split(os.sep)[-1]
+    #current_path = '../data/IMG/' + filename
+    current_path = 'templates/IMG/' + filename
+    image = cv2.cvtColor(cv2.imread(current_path), cv2.COLOR_BGR2RGB)
+    return image
+
+def getFlipped(image, steering):
+    image_flipped = np.fliplr(image)
+    measurement_flipped = -steering
+    return [image_flipped, measurement_flipped]
+
+
 images = []
 measurements = []
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
-    image = cv2.imread(current_path)
+    filename_center = line[0]
+    filename_left = line[1]
+    filename_right = line[2]
+    steering_center = float(line[3])
+
+    # create adjusted steering measurements for the side camera images
+    correction = 0.15  # this is a parameter to tune
+    steering_left = steering_center + correction
+    steering_right = steering_center - correction
+
+    image = loadImage(filename_center)
     images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
-    image_flipped = np.fliplr(image)
-    measurement_flipped = -measurement
+    measurements.append(steering_center)
+
+    [image_flipped, measurement_flipped] = getFlipped(image, steering_center)
+    images.append(image_flipped)
+    measurements.append(measurement_flipped)
+
+    image = loadImage(filename_left)
+    images.append(image)
+    measurements.append(steering_left)
+
+    [image_flipped, measurement_flipped] = getFlipped(image, steering_left)
+    images.append(image_flipped)
+    measurements.append(measurement_flipped)
+
+    image = loadImage(filename_right)
+    images.append(image)
+    measurements.append(steering_right)
+
+    [image_flipped, measurement_flipped] = getFlipped(image, steering_right)
     images.append(image_flipped)
     measurements.append(measurement_flipped)
 
@@ -32,18 +69,26 @@ y_train = np.array(measurements)
 
 
 from keras.models import Sequential
+from keras.callbacks import EarlyStopping
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D
 
 model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,24), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(24, (5, 5), activation='relu'))
+model.add(Conv2D(36, (5, 5), activation='relu'))
+model.add(Conv2D(48, (5, 5), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+callback = EarlyStopping(monitor='loss', patience=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3, callbacks = [callback])
 
 model.save('model.h5')
